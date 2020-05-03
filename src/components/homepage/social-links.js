101d150eacf4dde6e247a791bd9551c468616e33
Roman Glushko <r.glushko@atwix.com>
2020-05-03 19:29:33 +0300

    Added title attributes to links

--- src/components/homepage/social-links.js
+++ src/components/homepage/social-links.js
@@ -10,10 +10,10 @@ const SocialLinks = () => {
   return (
     <div className="social">
         <ul className="social-list">
-            <li className="social-item"><a href="https://twitter.com/roma_glushko" title="Twitter" target="blank"><FontAwesomeIcon icon={faTwitter} size="sm" /></a></li>
-            <li className="social-item"><a href="https://github.com/roma-glushko" title="Github" target="blank"><FontAwesomeIcon icon={faGithubAlt} size="sm" /></a></li>
-            <li className="social-item"><a href="https://www.linkedin.com/in/glushko-roman" title="LinkedIn" target="blank"><FontAwesomeIcon icon={faLinkedinIn} size="sm" /></a></li>
-            <li className="social-item"><a href="mailto:roman.glushko.m@gmail.com" title="Email"><FontAwesomeIcon icon={faEnvelope} size="sm" /></a></li>
+            <li className="social-item"><a href="https://twitter.com/roma_glushko" title="Roman Glushko on Twitter" target="blank"><FontAwesomeIcon icon={faTwitter} size="sm" /></a></li>
+            <li className="social-item"><a href="https://github.com/roma-glushko" title="Roman Glushko on Github" target="blank"><FontAwesomeIcon icon={faGithubAlt} size="sm" /></a></li>
+            <li className="social-item"><a href="https://www.linkedin.com/in/glushko-roman" title="Roman Glushko on LinkedIn" target="blank"><FontAwesomeIcon icon={faLinkedinIn} size="sm" /></a></li>
+            <li className="social-item"><a href="mailto:roman.glushko.m@gmail.com" title="Roman Glushko's Email"><FontAwesomeIcon icon={faEnvelope} size="sm" /></a></li>
         </ul>
     </div>
   );
