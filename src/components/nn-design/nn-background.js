28673e0a3e6ce5983b6fc355a3c72ef604fee62f
Roman Glushko <roman.glushko.m@gmail.com>
2021-01-05 14:23:43 +0200

    Made canvas worked with element level event listening && refactored the code

--- src/components/nn-design/nn-background.js
+++ src/components/nn-design/nn-background.js
@@ -5,15 +5,37 @@ import 'gsap/CSSPlugin'
 
 import "./hero-header.css"
 
+class Circle {
+  // constructor
+  constructor(ctx, pos, rad, color) {
+    this.drawCtx = ctx
+    this.pos = pos || null;
+    this.radius = rad || null;
+    this.color = color || null;
+  }
+
+  draw = function() {
+      if (!this.active) return
+
+      this.drawCtx.beginPath()
+      this.drawCtx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI, false)
+      this.drawCtx.fillStyle = 'rgba(31, 43, 49, ' + this.active + ')'
+      this.drawCtx.fill()
+  }
+}
+
 class NNBackground extends React.Component {
   
   componentDidMount() {
-    debugger
-
     (function() {
       
+      const pointSize = 3
+      const pointDensity = 20
+      const kNeighbors = 6
+
       // https://codepen.io/MarcoGuglielmelli/pen/lLCxy
-      var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+      let width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+      let mouseControl = false
     
       // Main
       initHeader();
@@ -21,179 +43,212 @@ class NNBackground extends React.Component {
       addListeners();
     
       function initHeader() {
-          width = window.innerWidth;
-          height = window.innerHeight;
-          target = {x: width/2, y: height/2};
+          //width = window.innerWidth
+          //height = window.innerHeight
+
+          // target = {
+          //   x: width/2, 
+          //   y: height/2
+          // }
     
-          largeHeader = document.getElementById('hero-header');
-          largeHeader.style.height = height+'px';
+          largeHeader = document.getElementById('hero-header')
+          //largeHeader.style.height = height+'px'
+
+          width = largeHeader.offsetWidth
+          height = largeHeader.offsetHeight
+
+          target = {
+            x: width * 0.5, 
+            y: height * 0.5
+          }
     
-          canvas = document.getElementById('neural-network-background');
-          canvas.width = width;
-          canvas.height = height;
-          ctx = canvas.getContext('2d');
+          canvas = document.getElementById('neural-network-background')
+          canvas.width = width
+          canvas.height = height
+          ctx = canvas.getContext('2d')
     
           // create points
-          points = [];
-          for(var x = 0; x < width; x = x + width/20) {
-              for(var y = 0; y < height; y = y + height/20) {
-                  var px = x + Math.random()*width/20;
-                  var py = y + Math.random()*height/20;
-                  var p = {x: px, originX: px, y: py, originY: py };
-                  points.push(p);
+          points = []
+
+          for (let x = 0; x < width; x = x + width / pointDensity) {
+              for (let y = 0; y < height; y = y + height / pointDensity) {
+                  let px = x + Math.random() * width / pointDensity,
+                      py = y + Math.random() * height / pointDensity
+                  
+                  points.push({
+                    x: px, 
+                    originX: px, 
+                    y: py, 
+                    originY: py
+                  })
               }
           }
     
           // for each point find the 5 closest points
-          for(var i = 0; i < points.length; i++) {
-              var closest = [];
-              var p1 = points[i];
-              for(var j = 0; j < points.length; j++) {
-                  var p2 = points[j]
-                  if(!(p1 == p2)) {
-                      var placed = false;
-                      for(var k = 0; k < 5; k++) {
-                          if(!placed) {
-                              if(closest[k] == undefined) {
-                                  closest[k] = p2;
-                                  placed = true;
-                              }
-                          }
-                      }
-    
-                      for(var k = 0; k < 5; k++) {
-                          if(!placed) {
-                              if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                  closest[k] = p2;
-                                  placed = true;
-                              }
-                          }
-                      }
+          for (let i = 0; i < points.length; i++) {
+              let closest = []
+              let p1 = points[i]
+
+              for (let j = 0; j < points.length; j++) {
+                let p2 = points[j]
+                
+                if (p1 == p2) {
+                    continue
+                }
+
+                var placed = false
+
+                for (var k = 0; k < kNeighbors; k++) {
+                  if (placed) continue
+
+                  if (closest[k] == undefined) {
+                    closest[k] = p2
+                    placed = true
                   }
+                }
+
+                for (var k = 0; k < kNeighbors; k++) {
+                  if (placed) continue
+
+                  if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                    closest[k] = p2
+                    placed = true
+                  }
+                }
               }
-              p1.closest = closest;
+
+              p1.closest = closest
           }
     
           // assign a circle to each point
-          for(var i in points) {
-              var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
-              points[i].circle = c;
+          for (var i in points) {
+              points[i].circle = new Circle(ctx, points[i], pointSize + Math.random() * pointSize, 'rgba(255, 255, 255, 0.3)')
           }
       }
     
       // Event handling
       function addListeners() {
-          if(!('ontouchstart' in window)) {
-              window.addEventListener('mousemove', mouseMove);
+          if (!('ontouchstart' in window)) {
+            canvas.addEventListener('mousemove', mouseMove);
+            canvas.addEventListener('mouseout', mouseOut);
           }
-          window.addEventListener('scroll', scrollCheck);
+
+          //window.addEventListener('scroll', scrollCheck);
           window.addEventListener('resize', resize);
       }
     
+      function mouseOut(e) {
+        mouseControl = false
+      }
+
       function mouseMove(e) {
           let posx = 0, posy = 0
 
-          if (e.pageX || e.pageY) {
-              posx = e.pageX;
-              posy = e.pageY;
-          }
-          else if (e.clientX || e.clientY)    {
-              posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-              posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-          }
-          target.x = posx;
-          target.y = posy;
+          mouseControl = true
+
+          // if (e.pageX || e.pageY) {
+          //   posx = e.pageX
+          //   posy = e.pageY
+          // } else if (e.clientX || e.clientY) {
+          //posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft
+          //posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop
+          //}
+
+          let rect = e.target.getBoundingClientRect()
+
+          posx = e.clientX - rect.left; //x position within the element.
+          posy = e.clientY - rect.top;  //y position within the element.
+          
+          target.x = posx
+          target.y = posy
       }
     
       function scrollCheck() {
-          if(document.body.scrollTop > height) animateHeader = false;
-          else animateHeader = true;
+          if (document.body.scrollTop > height) animateHeader = false
+          else animateHeader = true
       }
     
       function resize() {
-          width = window.innerWidth;
-          height = window.innerHeight;
-          largeHeader.style.height = height+'px';
-          canvas.width = width;
-          canvas.height = height;
+          //width = window.innerWidth
+          //height = window.innerHeight
+          //largeHeader.style.height = height+'px'
+          width = largeHeader.offsetWidth
+          height = largeHeader.offsetHeight
+
+          canvas.width = width
+          canvas.height = height
       }
     
       // animation
       function initAnimation() {
-          animate();
-          for(var i in points) {
+          animate()
+
+          for (var i in points) {
               shiftPoint(points[i]);
           }
       }
     
       function animate() {
-          if(animateHeader) {
-              ctx.clearRect(0,0,width,height);
-              for(var i in points) {
-                  // detect points in range
-                  if(Math.abs(getDistance(target, points[i])) < 4000) {
-                      points[i].active = 0.3;
-                      points[i].circle.active = 0.6;
-                  } else if(Math.abs(getDistance(target, points[i])) < 20000) {
-                      points[i].active = 0.1;
-                      points[i].circle.active = 0.3;
-                  } else if(Math.abs(getDistance(target, points[i])) < 40000) {
-                      points[i].active = 0.02;
-                      points[i].circle.active = 0.1;
-                  } else {
-                      points[i].active = 0;
-                      points[i].circle.active = 0;
-                  }
-    
-                  drawLines(points[i]);
-                  points[i].circle.draw();
-              }
+          if (!animateHeader) {
+            return  
           }
-          requestAnimationFrame(animate);
+
+          ctx.clearRect(0, 0, width, height)
+
+          for (var i in points) {
+              // detect points in range
+              if (Math.abs(getDistance(target, points[i])) < 4000) {
+                  points[i].active = 0.3;
+                  points[i].circle.active = 0.5;
+              } else if (Math.abs(getDistance(target, points[i])) < 20000) {
+                  points[i].active = 0.1;
+                  points[i].circle.active = 0.3;
+              } else if (Math.abs(getDistance(target, points[i])) < 40000) {
+                  points[i].active = 0.02;
+                  points[i].circle.active = 0.1;
+              } else {
+                  points[i].active = 0;
+                  points[i].circle.active = 0;
+              }
+
+              drawLines(points[i])
+              points[i].circle.draw()
+          }
+
+          requestAnimationFrame(animate)
       }
     
       function shiftPoint(p) {
-          TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
-              y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
-              onComplete: function() {
-                  shiftPoint(p);
-              }});
+        TweenLite.to(
+          p,
+          1 + 1 * Math.random(), 
+          {
+            x: p.originX - 50 + Math.random() * 100,
+            y: p.originY - 50 + Math.random() * 100, 
+            ease: Circ.easeInOut,
+            onComplete: function() {
+                shiftPoint(p);
+            }
+          }
+        )
       }
     
       // Canvas manipulation
       function drawLines(p) {
-          if(!p.active) return;
-          for(var i in p.closest) {
-              ctx.beginPath();
-              ctx.moveTo(p.x, p.y);
-              ctx.lineTo(p.closest[i].x, p.closest[i].y);
-              ctx.strokeStyle = 'rgba(31,43,49,'+ p.active+')';
-              ctx.stroke();
+          if (!p.active) return
+
+          for (var i in p.closest) {
+              ctx.beginPath()
+              ctx.moveTo(p.x, p.y)
+              ctx.lineTo(p.closest[i].x, p.closest[i].y)
+              ctx.strokeStyle = 'rgba(31, 43, 49, ' + p.active + ')'
+              ctx.stroke()
           }
-      }
-    
-      function Circle(pos,rad,color) {
-          var _this = this;
-    
-          // constructor
-          (function() {
-              _this.pos = pos || null;
-              _this.radius = rad || null;
-              _this.color = color || null;
-          })();
-    
-          this.draw = function() {
-              if(!_this.active) return;
-              ctx.beginPath();
-              ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-              ctx.fillStyle = 'rgba(31,43,49,'+ _this.active+')';
-              ctx.fill();
-          };
       }
     
       // Util
       function getDistance(p1, p2) {
-          return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
       }
       
     })();
