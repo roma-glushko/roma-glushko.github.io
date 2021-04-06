52e01eae78f808362ce3f3583d9b57ece08bbce2
Roman Glushko <roman.glushko.m@gmail.com>
2021-04-06 18:42:50 +0300

    Preloaded heavy MathJax resources

--- src/components/blog/mathjax.js
+++ src/components/blog/mathjax.js
@@ -26,6 +26,10 @@ class MathJax extends React.Component {
   render () {
     return <div className="mathjax">
       <Helmet>
+        {/** performance and UX improvement */}
+        <link rel="preload" as="script" href="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/jax/output/HTML-CSS/fonts/STIX/fontdata.js?V=2.7.5" />
+        <link rel="preload" as="script" href="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/jax/output/HTML-CSS/fonts/STIX/General/Italic/MathItalic.js?V=2.7.5" />
+
         {/** hack from https://github.com/hanai/gatsby-remark-mathjax/issues/1 */}
         <script
           type="text/javascript"
