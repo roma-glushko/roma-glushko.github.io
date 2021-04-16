f9fab53828f244754973d2252a81b2172c610641
Roman Glushko <roman.glushko.m@gmail.com>
2021-04-16 19:12:27 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-de5d9587a4e2eb06c36a.js"
+    "url": "webpack-runtime-9f2406119e88958543ae.js"
   },
   {
     "url": "styles.f14b81060f62ed5dfbd5.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4f20e42cf4c27be8ffff328e671347f2"
+    "revision": "574b6433d070a2761543a7dc5c933bf7"
   },
   {
     "url": "static/webfonts/s/dancingscript/v16/If2cXTr6YS-zF4S-kcSWSVi_sxjsohD9F50Ruu7BMSo3Sup8.woff2"
