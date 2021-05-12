ea08148f524c2f1ab89ea6c5abf401e9f5994316
Roman Glushko <roman.glushko.m@gmail.com>
2021-05-12 15:44:07 +0300

    Lazy loaded. iframes

--- src/components/blog/newsletter-form.js
+++ src/components/blog/newsletter-form.js
@@ -6,7 +6,8 @@ import "./newsletter-form.css"
 const NewsletterForm = () => {
   return (
     <div className="newsletter-form">
-      <iframe src="https://romanglushko.substack.com/embed" 
+      <iframe src="https://romanglushko.substack.com/embed"
+        loading="lazy"
         width="100%" height="320" 
         style={{border: '1px solid #EEE'}}
         frameborder="0" scrolling="no"
