eb5cce1e4db444eb1264620c8a20c109b5a0fdf8
Roman Glushko <roman.glushko.m@gmail.com>
2021-05-24 16:28:06 +0300

    Added information about me and styled recent blog post section

--- src/pages/indexv2.js
+++ src/pages/indexv2.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/theme/layout"
 import SEO from "../components/seo"
@@ -7,9 +8,10 @@ import Avatar from "../components/homepage/avatar"
 import ThemeSwitcher from "../components/theme/theme-switcher"
 import ReactRotatingText from "react-rotating-text"
 import MainNavigation from "../components/theme/main-navigation"
+import BlogTeaser from "../components/blog/blog-teaser"
+import ThoughtTeaser from "../components/thoughts/thought-teaser"
 import SocialLinks from "../components/homepage/social-links"
 import NNBackground from "../components/nn-design/nn-background"
-import { Link } from "gatsby"
 
 import "./indexv2.css"
 
@@ -38,7 +40,73 @@ const titles = shuffleArray([
     'Rookie Hacker 🔐',
 ])
 
-const IndexPage = () => (
+const IndexPage = () => {
+    const {recentPosts, recentThoughts} = useStaticQuery(
+        graphql`
+          query {
+            recentPosts: allMarkdownRemark(
+                sort: {order: DESC, fields: [frontmatter___date]}, 
+                limit: 2, 
+                filter: {
+                    fileAbsolutePath: {regex: "/(blog)/"}, 
+                    frontmatter: {published: {eq: true}}
+                }) {
+                    edges {
+                        node {
+                            id
+                            timeToRead
+                            frontmatter {
+                            humanDate: date(formatString: "MMM D, YYYY")
+                            fullDate: date(formatString: "YYYY-MM-DD")
+                            path
+                            title
+                            keywords
+                            excerpt
+                            cover {
+                                childImageSharp {
+                                  fluid(maxWidth: 400) {
+                                    ...GatsbyImageSharpFluid
+                                  }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            recentThoughts: allMarkdownRemark(
+                    sort: {order: DESC, fields: [frontmatter___date]}, 
+                    limit: 2, 
+                    filter: {
+                        fileAbsolutePath: {regex: "/(thoughts)/"}, 
+                        frontmatter: {published: {eq: true}}
+                    }) {
+                        edges {
+                            node {
+                                id
+                                timeToRead
+                                excerpt(pruneLength: 100)
+                                frontmatter {
+                                    humanDate: date(formatString: "MMM D, YYYY")
+                                    fullDate: date(formatString: "YYYY-MM-DD")
+                                    path
+                                    title
+                                    keywords
+                                    cover {
+                                        childImageSharp {
+                                        fluid(maxWidth: 400) {
+                                            ...GatsbyImageSharpFluid
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+          }
+        `
+      )
+    
+    return (
     <Layout>
         <SEO title="Roman Glushko - Machine Learning Engineer, Magento eCommerce Software Developer and Life Explorer" isUniqueTitle={true} className="home-page-v2" pagePath="/" />
         <main className="homepagev2">
@@ -65,9 +133,76 @@ const IndexPage = () => (
                     </figure>
                 </div>
             </div>
+            <div className="mylife">
+                <div>
+                    <p className="hey">Hey,</p>
+                    <p>My name is Roman 👋</p>
+                    <p>I'm a Machine Learning and Software Engineer, Science Geek and Life Explorer.</p>
+                </div>
+                <div>
+                    <h2 className="activity-title">Learn & Share</h2>
+                    <div>
+                        <p>I'm a life-time learner interested in a broad variety of topics: Machine and Deep Learning, Science-related Theory, Computer Science, Software Engineering, Distributed System Design, eCommerce and so on.</p>
+                        <p>When I have spare time, I enjoy putting together my knowledge about those topics and share with other people. </p>
+                        <div className="recent-posts">
+                            {recentPosts.edges.map(({node}) => (
+                                <BlogTeaser
+                                    key={node.id}
+                                    title={node.frontmatter.title}
+                                    url={node.frontmatter.path}
+                                    timeToRead={node.timeToRead}
+                                    publishedHumanDate={node.frontmatter.humanDate}
+                                    publishedFullDate={node.frontmatter.fullDate}
+                                    excerpt={node.frontmatter.excerpt}
+                                    cover={node.frontmatter.cover}
+                                    keywords={node.frontmatter.keywords}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2 className="activity-title">Think & Write</h2>
+                    <div>
+                        <p>Another thing I like to do is to explore this worlds, think about its components, their interconnections and finally capture my observations and thoughts. </p>
+                        <p>There thoughts are usually about self-development, management and people perceptions and mindsets.</p>
+
+                        <div className="recent-thoughts">
+                            {recentThoughts.edges.map(({node}) => (
+                                <ThoughtTeaser
+                                    key={node.id}
+                                    title={node.frontmatter.title}
+                                    url={node.frontmatter.path}
+                                    timeToRead={node.timeToRead}
+                                    publishedHumanDate={node.frontmatter.humanDate}
+                                    publishedFullDate={node.frontmatter.fullDate}
+                                    excerpt={node.excerpt}
+                                    cover={node.frontmatter.cover}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                {/* <div>
+                    <h2>Code & Open Source</h2>
+                </div> */}
+                {/* <div>
+                    <h2>Experiment & Deploy</h2>
+                </div> */}
+                <div>
+                    <h2 className="activity-title">Work & Help</h2>
+                    <div>
+                        <p>If you have a great challenge to solve with Machine Learning and Software Engineering, feel free to let me know! </p>
+                        <p>Here is my CVs, by the way: </p>
+
+                        Insert CVs here
+                    </div>
+                </div>
+            </div>
         </main>
         <PersonRichSnippet />
-    </Layout>
-)
+    </Layout>)
+}
+
 
 export default IndexPage
