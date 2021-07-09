2f3cd767d66e5f47755f702fc2592a60421581d6
Roman Glushko <roman.glushko.m@gmail.com>
2021-07-09 13:02:58 +0300

    Fixed typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ const titles = shuffleArray([
 
 const IndexPage = () => (
     <Layout>
-        <SEO title="Roman Glushko - Machine Learning and Software Engineer Developer" isUniqueTitle={true} className="home-page" pagePath="/" />
+        <SEO title="Roman Glushko - Machine Learning and Software Engineer" isUniqueTitle={true} className="home-page" pagePath="/" />
         <main className="container">
             <div className="row">
                 <div className="column homepage-sidebar sidebar">
