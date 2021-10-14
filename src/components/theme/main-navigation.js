6c182c5cc8a221960b374839912f5b88766d300d
Roman Glushko <roman.glushko.m@gmail.com>
2021-10-14 11:26:35 +0300

    Enabled Lab page in the main navigation

--- src/components/theme/main-navigation.js
+++ src/components/theme/main-navigation.js
@@ -5,16 +5,15 @@ import { Link } from "gatsby"
 import "./main-navigation.css"
 
 const MainNavigation = ({ space }) => {
-  // <li><Link to="/lab/" title="Go to Machine Learning Lab">Lab</Link></li>
-  
   return (
     <nav className="main-navigation">
       <ul>
         {space !== 'homepage' &&
           <li><Link rel="home" to="/" title="Go Home">Home</Link></li>
         }
-        <li><Link to="/blog/" title="Go to Technical blog">Blog</Link></li>
-        <li><Link to="/thoughts/" title="Go to Thoughts">Thoughts</Link></li>
+        <li><Link to="/blog/" title="Go to my Technical Blog">Blog</Link></li>
+        <li><Link to="/lab/" title="Go to my Lab">Lab</Link></li>
+        <li><Link to="/thoughts/" title="Go to my Thoughts">Thoughts</Link></li>
         <li><Link to="/cv/machine-learning-engineer/" title="Review My CVs">CV</Link></li>
       </ul>
     </nav>
