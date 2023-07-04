3ec0c7b37ac35b09a00784017aa107b8b2ec78fd
Roman Glushko <roman.glushko.m@gmail.com>
2023-07-05 00:42:04 +0300

    #42 Inited blog pod teasers and structure of the blog posts

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,12 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-
 import partytown from "@astrojs/partytown";
+
+import rmMath from 'remark-math';
+import rhKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.romaglushko.com/',
+  markdown: {
+    drafts: true,
+    remarkPlugins: [rmMath],
+    rehypePlugins: [rhKatex],
+  },
   integrations: [
     mdx(), 
     sitemap(), 
--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -1,20 +1,23 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
 	schema: z.object({
+		id: z.string().optional(),
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
+		excerpt: z.string(),
+		keywords: z.array(z.string()),
 		pubDate: z
 			.string()
 			.or(z.date())
 			.transform((val) => new Date(val)),
-		updatedDate: z
+		updateDate: z
 			.string()
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+		cover: z.object({
+			image: z.string(),
+			cover: z.string().optional(),
+		}),
 	}),
 });
 
