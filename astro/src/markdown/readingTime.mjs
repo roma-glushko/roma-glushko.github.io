ecd585628ed7771372af6d4c2dd7ba4f51ccfc00
Roman Glushko <roman.glushko.m@gmail.com>
2023-07-31 22:12:18 +0300

    Added the time to read data

--- astro/src/markdown/readingTime.mjs
+++ astro/src/markdown/readingTime.mjs
@@ -8,7 +8,7 @@ const RemarkReadingTime = () => {
         // readingTime.text will give us minutes read as a friendly string,
         // i.e. "3 min read"
 
-        data.astro.frontmatter.readingTimeMin = readingTime.text;
+        data.astro.frontmatter.timeToReadMin = readingTime.text;
     };
 }
 
