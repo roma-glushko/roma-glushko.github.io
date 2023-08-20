416e600ff63d4007dcb978a4b6fdfb8efe92c7b7
Roman Glushko <roman.glushko.m@gmail.com>
2023-08-20 19:04:11 +0300

    Extended the rss items and redirected sitemap URL

--- astro/src/pages/rss.xml.js
+++ astro/src/pages/rss.xml.js
@@ -1,19 +1,28 @@
 import rss from '@astrojs/rss';
 
-import { getCollection } from 'astro:content';
-
 import siteMetadata from "../data/siteMetadata";
+import { getBlogPosts, getThoughts } from 'src/content/collections';
+import { getBlogUrl, getThoughtUrl } from 'src/content/routing';
+
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	
+	const blogPosts = await getBlogPosts()
+	const thoughts = await getThoughts()
+
+	const blogItems = blogPosts.map((post) => ({
+		...post.data,
+		link: getBlogUrl(post.slug),
+	}))
+
+	const thoughtItems = thoughts.map((post) => ({
+		...post.data,
+		link: getThoughtUrl(post.slug),
+	}))
+
 	return rss({
 		title: siteMetadata.title,
 		description: siteMetadata.description,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: [...blogItems, ...thoughtItems],
 	});
 }
